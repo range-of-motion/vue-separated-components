dfaf6f97c9d207912726cd1b9f76b75f681b9cac
Daniël van Drunen <daniel@pixely.me>
2022-07-11 12:41:30 +0200

    Set up Vue and migrate back to Laravel Mix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+import Vue from 'vue';
+
+// Components
+import Counter from './components/Counter.vue';
+
+Vue.component('counter', Counter);
+
+new Vue({
+    el: '#app',
+});
---
+++ resources/js/components/Counter.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+
+const counter = ref(0);
+
+function increment() {
+    counter.value++;
+}
+</script>
+
+<template>
+    <div>
+        <h1>{{ counter }}</h1>
+        <button @click="increment">Increment</button>
+    </div>
+</template>
